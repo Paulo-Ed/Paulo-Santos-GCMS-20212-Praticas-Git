ecfcc034db6831ec4b497b702584e1def4f8219d
Paulo Santos <pauloed_@outlook.com>
2022-02-11 07:31:08 -0300

    Merge branch 'hotfix' into main

--- pratica2Git/src/pratica2Git/helloWorld.java
+++ pratica2Git/src/pratica2Git/helloWorld.java
@@ -16,13 +16,14 @@ public class helloWorld {
 		System.out.println("Oitavo commit!");
 		System.out.println("Nono commit!");
 		System.out.println("Décimo commit!");
+		System.out.println("Décimo primeiro commit!");
+		
 		
 		System.out.println("Primeiro commit branch hotfix!");
 		System.out.println("Segundo commit branch hotfix!");
 		System.out.println("Terceiro commit branch hotfix!");
 		System.out.println("Quarto commit branch hotfix!");
 		
-		System.out.println("Décimo primeiro commit!");
 		
 
 	
